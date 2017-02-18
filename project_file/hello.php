bc7721fc606534e4f59e547bebfaa17bc4c0ac47
Sahapap <sahapap_panya@cmu.ac.th>
2017-02-18 12:39:34 +0700

    validate

--- project_file/hello.php
+++ project_file/hello.php
@@ -1,8 +1,23 @@
 <?php
-echo "Hello World";
+echo "Hello World <br>";
 //print("\n")
-echo "\n";
+//echo "\n";
 echo $_POST["Lname"] ;
 echo " ";
-echo $_POST["Sname"];
+echo $_POST["Sname"]."<br>";
+
+$x = $_POST['Sid'];
+
+//echo $x; 
+
+if (strlen($x) == 13){
+    echo $x;
+}else{
+    echo '<script>alert("error");</script>';
+    sleep(5);
+    echo'<script>window.location="./InputInformation.html";</script>';
+ //   header('Location: ./InputInformation.html');
+}
+
+
 ?>
